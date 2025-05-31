9eb240d40fc2ab237ca32e7eb907ab8eb3f3a362
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2025-05-31 18:11:41 +0200

    basic setup, redux, localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,45 @@
-import Heading from './components/Heading/Heading';
+import { Suspense, useEffect } from 'react';
+import Header from './components/Header/Header';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Loader from './components/Loader/Loader';
+import { lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchBaseCurrency } from './redux/operations';
+
+const Home = lazy(() => import('./pages/Home'));
+const Rates = lazy(() => import('./pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      dispatch(fetchBaseCurrency(pos.coords))
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, [dispatch]);
+  return (
+    <>
+      <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rates" element={<Rates />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store.js';
 
 import { App } from './App.jsx';
 
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
